f14e1741e6f635483748259f894f230754e22900
Ryudenshi <skarydog17@gmail.com>
2023-04-24 10:13:58 +0300

    image adding don	 work

--- app/Http/Controllers/projectController.php
+++ app/Http/Controllers/projectController.php
@@ -33,8 +33,26 @@ class projectController extends Controller
     public function store(Request $request)
     {
         
-
         
+        $this->validate($request, [
+            'image' => 'required|image|mimes:jpg,png,jpeg,gif,svg|max:2048',
+            'title' => 'required',
+            'description' => 'required',
+            'category_id' => 'required',
+            'header' => 'required'
+        ]);
+
+        if ($request->hasFile('image') && $request->file('image')->isValid()) {
+            $imagePath = $request->file('image')->store('public/images');
+            $project = new Project();
+        
+            $project->image = $imagePath;
+            $project->title = $request->input('title');
+            $project->description = $request->input('description');
+            $project->category_id =$request->input('category_id');
+            $project->header = $request->input('header');
+            $project->save();
+        }
 
         return redirect('/projects');
     }
