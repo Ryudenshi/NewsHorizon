18d4baf80a5d61b387447836b4f8144f35c12d7d
Ryudenshi <skarydog17@gmail.com>
2023-04-25 11:11:39 +0300

    text dispaying fix

--- resources/views/projects/project.blade.php
+++ resources/views/projects/project.blade.php
@@ -4,18 +4,23 @@
 
 <div class="container my-5">
     <div class="col">
-        <div class="row">
+        <div class="row projectTitle" style="border-bottom: 1px solid #c0c0c0;">
             <h1>{{ ($project->title) }}</h1>
         </div>
-        <div class="row justify-content-center">
-            <img class="projectImage" style="width: 70%" src="{{ asset('storage/' . $project->image) }}" alt="Project Image">
+        <div class="my-3 row justify-content-center">
+            <img class="" style="width: 70%" src="{{ asset('storage/' . $project->image) }}" alt="Project Image">
         </div>
-        <div class="row">
+        <div class="row" style="border-top: 1px solid #c0c0c0;">
             <h5>{{ ($project->header) }}</h3>
         </div>
-
-
-        <p>{{ ($project->description) }}</p>
+        <div class="row justify-content-center">
+            <div class="mt-3" style="
+            font-size: 20px;
+            width: 700px;
+            ">
+                <p>{{ ($project->description) }}</p>
+            </div>
+        </div>
     </div>
 </div>
 
